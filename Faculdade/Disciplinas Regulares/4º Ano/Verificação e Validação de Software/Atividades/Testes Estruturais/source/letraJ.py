bc90378dc7695a548fa91694d2131b0ce0d9e9cc
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-06-08 18:55:47 -0300

    Realizando alterações no algoritmo do 5] exercício.

--- Faculdade/Disciplinas Regulares/4º Ano/Verificação e Validação de Software/Atividades/Testes Estruturais/source/letraJ.py
+++ Faculdade/Disciplinas Regulares/4º Ano/Verificação e Validação de Software/Atividades/Testes Estruturais/source/letraJ.py
@@ -1,28 +1,11 @@
 cod = int(input('Informe o código do empregado: '))
 nasc = int(input('Informe o ano de nascimento: '))
 ing = int(input('Informe o ano de ingresso: '))
+
+idd = 2021 - nasc
 trab = 2021 - ing
 
-
-if (2021 - nasc >= 65) or (trab >= 30) or ((2021 - nasc >= 60) and trab >= 25):
-    print('Idade: {} anos - Tempo de Trabalho: {} anos. Requerer aposentadoria.'.format((2021-nasc), trab))
+if (idd >= 65) or (trab >= 30) or ((idd >= 60) and trab >= 25):
+    print('{} anos - Tempo de Trabalho: {} anos. Requerer aposentadoria.'.format((idd), trab))
 else:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos. Não requerer aposentadoria.'.format((2021-nasc), trab))
-
-
-if (cod < 0) or (nasc < 0) or (ing < 0):
-    print('Há um ou mais dados incorretos informados.')
-    print()
-
-    while (cod < 0) or (nasc < 0) or (ing < 0):
-
-        cod = int(input('Informe o código do empregado: '))
-        nasc = int(input('Informe o ano de nascimento: '))
-        ing = int(input('Informe o ano de ingresso: '))
-        trab = 2021 - ing
-
-        if (2021 - nasc >= 65) or (trab >= 30) or ((2021 - nasc >= 60) and trab >= 25):
-            print(
-                'Idade: {} anos - Tempo de Trabalho: {} anos. Requerer aposentadoria.'.format((2021-nasc), trab))
-        else:
-            print('Idade: {} anos - Tempo de Trabalho: {} anos. Não requerer aposentadoria.'.format((2021-nasc), trab))
+    print('{} anos - Tempo de Trabalho: {} anos. Não requerer.'.format((idd), trab))
