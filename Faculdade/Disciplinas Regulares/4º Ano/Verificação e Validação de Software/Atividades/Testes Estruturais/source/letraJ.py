9a86a12236d3f86e0ef42651d1315457a5fad1dd
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-06-08 13:32:30 -0300

    Limpando o código.

--- Faculdade/Disciplinas Regulares/4º Ano/Verificação e Validação de Software/Atividades/Testes Estruturais/source/letraJ.py
+++ Faculdade/Disciplinas Regulares/4º Ano/Verificação e Validação de Software/Atividades/Testes Estruturais/source/letraJ.py
@@ -3,11 +3,26 @@ nasc = int(input('Informe o ano de nascimento: '))
 ing = int(input('Informe o ano de ingresso: '))
 trab = 2021 - ing
 
-if 2021 - nasc >= 65:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.'.format((2021 - nasc), trab))
-elif trab >= 30:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.'.format((2021 - nasc), trab))
-elif (2021 - nasc >= 60) and trab >= 25:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.'.format((2021 - nasc), trab))
+
+if (2021 - nasc >= 65) or (trab >= 30) or ((2021 - nasc >= 60) and trab >= 25):
+    print('Idade: {} anos - Tempo de Trabalho: {} anos. Requerer aposentadoria.'.format((2021-nasc), trab))
 else:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Não requerer aposentadoria.'.format((2021 - nasc), trab))
+    print('Idade: {} anos - Tempo de Trabalho: {} anos. Não requerer aposentadoria.'.format((2021-nasc), trab))
+
+
+if (cod < 0) or (nasc < 0) or (ing < 0):
+    print('Há um ou mais dados incorretos informados.')
+    print()
+
+    while (cod < 0) or (nasc < 0) or (ing < 0):
+
+        cod = int(input('Informe o código do empregado: '))
+        nasc = int(input('Informe o ano de nascimento: '))
+        ing = int(input('Informe o ano de ingresso: '))
+        trab = 2021 - ing
+
+        if (2021 - nasc >= 65) or (trab >= 30) or ((2021 - nasc >= 60) and trab >= 25):
+            print(
+                'Idade: {} anos - Tempo de Trabalho: {} anos. Requerer aposentadoria.'.format((2021-nasc), trab))
+        else:
+            print('Idade: {} anos - Tempo de Trabalho: {} anos. Não requerer aposentadoria.'.format((2021-nasc), trab))
