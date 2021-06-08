e920e736bbe6166d48cd183353f5211bf94eb14d
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-06-08 13:18:35 -0300

    Fazendo correções no quinto algoritmo.

--- Faculdade/Disciplinas Regulares/4º Ano/Verificação e Validação de Software/Atividades/Testes Estruturais/source/letraJ.py
+++ Faculdade/Disciplinas Regulares/4º Ano/Verificação e Validação de Software/Atividades/Testes Estruturais/source/letraJ.py
@@ -4,10 +4,10 @@ ing = int(input('Informe o ano de ingresso: '))
 trab = 2021 - ing
 
 if 2021 - nasc >= 65:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.')
+    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.'.format((2021 - nasc), trab))
 elif trab >= 30:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.')
+    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.'.format((2021 - nasc), trab))
 elif (2021 - nasc >= 60) and trab >= 25:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.')
+    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Requerer aposentadoria.'.format((2021 - nasc), trab))
 else:
-    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Não requerer aposentadoria.')
+    print('Idade: {} anos - Tempo de Trabalho: {} anos - Situacão: Não requerer aposentadoria.'.format((2021 - nasc), trab))
